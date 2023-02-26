c72b2443becc1d83279d19a92bc748cc27ec51fa
danielkwambai <57962011+danielkwambai@users.noreply.github.com>
2023-02-26 13:40:04 +0000

    creation of views.py

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-# Create your views here.
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+    
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'Email Already Exists')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'Username Already Exists')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username,email=email,password=password)
+                user.save();
+                return redirect('login')
+        else:
+            messages.info(request,'Password Not The Same')
+            return redirect
