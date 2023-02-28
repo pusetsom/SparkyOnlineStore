26a11ee4e9a59ab1c65772430a53d2f2a7a6962e
Kaybee-debug <87021754+Kaybee-debug@users.noreply.github.com>
2023-02-28 06:56:24 +0000

    register model

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
 def index(request):
-    return render(request,"home/index.html")
+    items = Product.objects.all()
+    
+    context = {
+        "items":items
+    }
+    
+   
+   
+    return render(request,"index.html",context)
+
+def add_product(request):
+  
+    form = ProductForm()
+    if request.method == "POST":
+        form = ProductForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+    
+    
+    
+    context = {
+        "form":form
+    }
+   
+    return render(request,"create.html",context)
